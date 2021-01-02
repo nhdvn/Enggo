19a534da11693673c7d8d450ed1f5345d175f893
Nguyen-Tuan Anh <nguyentuananh.qpc@gmail.com>
2021-01-02 20:23:11 +0700

    Login and signup UI

--- app/src/main/java/com/example/enggo/MainActivity.java
+++ app/src/main/java/com/example/enggo/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.enggo;
-package test;
+
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import Login.login;
 
 public class MainActivity extends AppCompatActivity
 {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent itt = new Intent(MainActivity.this , login.class);
+        startActivity(itt);
+
     }
 }

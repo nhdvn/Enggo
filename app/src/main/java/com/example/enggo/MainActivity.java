723e2147c8f628f045156854bd35103fd485a950
Nguyen-Tuan Anh <nguyentuananh.qpc@gmail.com>
2021-01-02 20:52:43 +0700

    I dont Know

--- app/src/main/java/com/example/enggo/MainActivity.java
+++ app/src/main/java/com/example/enggo/MainActivity.java
@@ -13,15 +13,11 @@ import Login.login;
 
 public class MainActivity extends AppCompatActivity
 {
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent itt = new Intent(MainActivity.this , login.class);
+        Intent itt = new Intent(MainActivity.this, login.class);
         startActivity(itt);
-
     }
 }
